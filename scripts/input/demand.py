b2df3ce814946d9230ab87e177d52bf55a38f4ff
Shai Benjamin <sheyeh@gmail.com>
2022-04-13 20:22:41 +0300

    Add comment about industy data

--- scripts/input/demand.py
+++ scripts/input/demand.py
@@ -5,6 +5,8 @@ efficiency_targets_pct = {2020: 0, 2030: 7, 2040: 15, 2050: 25}  # [%]
 yearly_growth_pct = {2020: 2.8, 2030: 2.8, 2040: 2.8, 2050: 2.8}  # [%]
 
 # Industry
+# Assumes a baseline value and growth+efficiency+conversion.
+# TODO: get more recent data from ecotraders.co.il
 industry_conversion_factor = 0.89
 industry_conversion_fossil_to_electricity_pct = {2020: 0, 2030: 10, 2040: 40, 2050: 80}
 industry_efficiency_reduction_pct = {2020: 0, 2030: 5, 2040: 15, 2050: 25}
