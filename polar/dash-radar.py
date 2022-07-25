ea19d4079c9fd016cc95e271a5f84deaaac9a959
Shai Benjamin <sheyeh@gmail.com>
2022-07-25 18:51:06 +0300

    Add vertical bar for year (currently not used) Remove option for radar plot (leave only bar)

--- polar/dash-radar.py
+++ polar/dash-radar.py
@@ -20,10 +20,12 @@ df['solar'] = df['wind'] + df['solarUsage']
 df['allsolar'] = df['solar'] + df['storageSolarCharge']
 df['curtailed'] = df['allsolar'] + df['curtailedEnergy']
 df['discharge'] = df['curtailed'] + df['storageDischarge']
-max_tick = math.ceil(df['discharge'].max()/20000)*20000
-tickvals = [i*20000 for i in range(int(max_tick/20000))]
-
 df['onlysolar'] = df['solarUsage'] + df['curtailedEnergy'] + df['storageDischarge']
+
+TICK_STEP = 20000
+max_tick = math.ceil(df['discharge'].max()/TICK_STEP)*TICK_STEP
+tickvals = [i*TICK_STEP for i in range(1, int(max_tick/TICK_STEP))]
+
 df_by_date = df.groupby(['date']).sum()
 print("Running")
 
@@ -73,27 +75,12 @@ def barplot(day_of_year):
     return f
 
 
-def radar(day_of_year):
-    f = go.Figure()
-    f.add_trace(polar_scatter(day_of_year, "discharge", "lightblue"))
-    f.add_trace(polar_scatter(day_of_year, "curtailed", "yellow"))
-    f.add_trace(polar_scatter(day_of_year, "allsolar", "gold"))
-    f.add_trace(polar_scatter(day_of_year, "solar", "orange"))
-    f.add_trace(polar_scatter(day_of_year, "wind", "lightgreen"))
-    f.add_trace(polar_scatter(day_of_year, "allgas", "silver"))
-    f.add_trace(polar_scatter(day_of_year, "gas", "lightgray"))
-    f.add_trace(polar_scatter(day_of_year, "coal", "black"))
-    f.add_trace(polar_scatter(day_of_year, "demand", "red", False))
-    f.add_trace(polar_scatter(day_of_year, "netDemand", "purple", False, True))
-    return f
-
-
-def plot(day_of_year, bar):
-    f = barplot(day_of_year) if bar else radar(day_of_year)
+def plot(day_of_year):
+    f = barplot(day_of_year)
     date = df['date'][day_of_year * 24]
     f.update_layout(
         title=go.layout.Title(
-            x=0.5,
+            x=0.45,
             xanchor='center',
             text=datetime.strptime(date, "%m/%d/%Y").strftime("%d %B, %Y")),
         height=800,
@@ -102,8 +89,8 @@ def plot(day_of_year, bar):
             radialaxis=dict(visible=True,
                             range=[0, max_tick],
                             tickvals=tickvals,
-                            tickfont=dict(color="white"),
-                            tickangle=90),
+                            tickfont=dict(color="black"),
+                            tickangle=30),
             angularaxis=dict(
                 tickfont_size=12,
                 rotation=270,
@@ -160,7 +147,6 @@ dash_app.layout = html.Div([
     html.H1('Daily generation and consumption', style={'textAlign': 'center'}),
     html.Div(
         style={'position': 'relative'},
-        # style={'backgroundColor':'white'},
         children=[
             html.Div(children=[
                 dcc.Graph(figure=heatmap('wind', [[0, 'white'], [1, 'red']]), config={'displayModeBar': False}),
@@ -175,21 +161,38 @@ dash_app.layout = html.Div([
                                step=1,
                                value=pd.Timestamp(2050, 6, 1).dayofyear - 1,
                                marks=marks(2050),
-                               id="slider",
+                               id="date-slider",
                                ),
                 ]),
         ],
     ),
-    dcc.Checklist(["Bar or Scatter"], id="bar-or-scatter"),
-    dcc.Graph(id="daily-radar-plot", config={'displayModeBar': False}),
+    html.Div(className="row", children=[
+        html.Div(
+            [dcc.Slider(
+                min=2020,
+                max=2050,
+                step=1,
+                value=2050,
+                marks={y: str(y) for y in range(2020, 2051, 10)},
+                id="year-slider",
+                vertical=True,
+                verticalHeight=600)],
+            style={'width': '7%', 'display': 'inline-block'}
+        ),
+        html.Div(children=[
+                dcc.Graph(id="daily-radar-plot", config={'displayModeBar': False})],
+            style={'width': '90%', 'display': 'inline-block', 'vertical-align': 'top'}
+        )
+    ])
 ])
 
 
 @dash_app.callback(
     Output("daily-radar-plot", "figure"),
-    [Input("slider", "value"), Input("bar-or-scatter", "value")])
-def update_radar(slider, bar):
-    fig = plot(slider, bar and "Bar or Scatter" in bar)
+    [Input("date-slider", "value"), Input("year-slider", "value")])
+def update_radar(day_of_year, year):
+    # year is ignored for now
+    fig = plot(day_of_year)
     return fig
 
 
